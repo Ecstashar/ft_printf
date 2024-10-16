c608e161b005a40ba530159ee325348745eb67e9
Sharvind Kumar <skumar@u91z01s02.42kl.edu.my>
2024-10-16 14:41:58 +0800

    initial commit

---
+++ ft_printf.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skumar <skumar@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 22:14:45 by skumar            #+#    #+#             */
+/*   Updated: 2024/10/16 14:30:24 by skumar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <stdint.h>
+
+size_t ft_strlen
+
+int ft_putchar
+int ft_putstr
+int ft_putptr
+int ft_printfloat
+int ft_putnbr
+
+
+
+
+#endif
