3d5765977c27cf4c22c3dc1d7f4bf948c479ec99
Sharvind Kumar <skumar@u81z08s01.42kl.edu.my>
2024-10-19 15:46:10 +0800

    2nd push with printc file

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: skumar <skumar@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 22:14:45 by skumar            #+#    #+#             */
-/*   Updated: 2024/10/16 14:30:24 by skumar           ###   ########.fr       */
+/*   Updated: 2024/10/18 21:59:49 by skumar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,9 @@ size_t ft_strlen
 int ft_putchar
 int ft_putstr
 int ft_putptr
-int ft_printfloat
 int ft_putnbr
+int ft_unsign
+int ft_hexa
 
 
 
